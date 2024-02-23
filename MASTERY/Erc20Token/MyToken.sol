e57c07300168f1268444225bf5dbebb8a1ac80d5
Jaz-3-0 <sebejaz99@gmail.com>
2024-02-23 15:47:45 +0200

    update fix-typos

--- MASTERY/Erc20Token/MyToken.sol
+++ MASTERY/Erc20Token/MyToken.sol
@@ -6,6 +6,6 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MyToken is ERC20 {
 
     constructor() ERC20("Mangocoin", "mango") {
-        _mint(msg.sender, 100 * 10 ** ERC20.decimals());methods that
+        _mint(msg.sender, 100 * 10 ** ERC20.decimals());
     }
 }
