6cf394d7f9e71c789cfff4ec890fcd62f5cc5fc3
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-04 18:11:02 +0200

    update init inherit

--- ADVANCING/ZTM.sol
+++ ADVANCING/ZTM.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 contract ZTM {
 
     address public myAddr = "0xba57bF26549F2Be7F69EC91E6a9db6Ce1e375390";
+    uint256 public balance;
 
     // Mappings
     mapping(keyType => valueType) public myMappingName;
@@ -56,14 +57,39 @@ contract ZTM {
     function _helperFunction() private returns(myNumber) {}
 
     // function Modifiers
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can access this);
+        _;
+    }
+
+    function changeOwner(address newOwner) external onlyOwner {
+        owner = newOwner;
+
+    }
+
+    /// Fallback functions
+    // Executed when called with empty data, must be external and payable
+    receive() external payable {}
+
+    // Executed when no no other function matches, must be external, can be payable
+    fallback() external {}
+
+    // Contracts
     
+    constructor(uint256 initialBalance) {
+        balance = initialBalance;
+    }
 
+    function setBalance(uint256 newBalance) external {
+        balance = newBalance;
+    }
 
+    // We can create instances from our contracts
+    ZTM ztm = new ZTM(100);
+    ZTM ztm2 = new ZTM(0xba57bF26549F2Be7F69EC91E6a9db6Ce1e375390);
+    //  this: current contract
+    // addres(this): current contract address
 
-
-
-
-    
 
     function transfer() public {
         myAddr.balance;
@@ -71,10 +97,28 @@ contract ZTM {
     // It's generally better to use call and handle reentrancy issues separately
         (bool success, ) = myAddr.call{value: 1 ether}("");
         require(success, "Transfer failed");
-
-
-
-
-
     }
 }
+
+// Inheritance
+
+contract MyAncestorContract2 {
+
+    function myFunction() external virtual {};
+
+}
+
+contract MyAncestorContract1 is MyAncestorContract2 {
+    function myFunction() external virtual override {};
+}
+
+contract MyContract is MyAncestorContract1 {
+    function myFunction() external override(MyAncestorContract1, MyAncestorContract2) {}
+
+}
+
+// Call first ancestor function
+super.myFunction();
+// Call specific ancestor function
+MyAncestorContract1.myFunction();
+MyAncestot
