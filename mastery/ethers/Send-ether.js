8fa666e8f5d5d9cdbc0f28c31fcbf586e2b60279
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-15 16:51:15 +0200

    update init resolve

--- mastery/ethers/Send-ether.js
+++ mastery/ethers/Send-ether.js
@@ -1,4 +1,6 @@
 const { ethers } = require("ethers");
+const dotenv = require("dotenv");
+require("dotenv").config();
 
 const sender_pvt_key = process.env.PRIVATE_KEY;
 const receiver_address = "0xcfdEf8165d722DC56a1FD06923de261B5B9d8626";
