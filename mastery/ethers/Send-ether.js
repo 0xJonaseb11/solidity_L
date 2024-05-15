592ef346b359c29809e84c40325d4bd5bb396f33
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-15 16:44:59 +0200

    update init ethers lib

---
+++ mastery/ethers/Send-ether.js
@@ -0,0 +1,19 @@
+const { ethers } = require("ethers");
+
+const sender_pvt_key = process.env.PRIVATE_KEY;
+const receiver_address = "0xcfdEf8165d722DC56a1FD06923de261B5B9d8626";
+const amount = 0.5;
+
+let provider = ethers.getDefaultProvider("sepolia");
+
+let sender_wallet = new ethers.Wallet(sender_pvt_key, provider);
+
+let txn = {
+    to: receiver_address,
+    value: ethers.utils.parseEther(amount)
+}
+
+// send txn
+sender_wallet.sendTransaction(txn).then((result) => {
+    console.log("TxHash:", result.hash);
+});
