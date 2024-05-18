26745c810701b5628e97c33a67f3d5439dfcf6a5
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-18 10:51:19 +0200

    update init resolve

---
+++ mastery/frameworks/PriceConsumerV3.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface internal priceFeed;
+
+
+   /**
+    * Network: Sepolia
+    * Aggregator: ETH/USD
+    * Address: 0x1a81afB8146aeFfCFc5E50e8479e826E7D55b910
+    */
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x1a81afB8146aeFfCFc5E50e8479e826E7D55b910);
+    }
+
+    /**
+    * returns the latest price 
+    */
+
+    function getLatestPrice() public view returns(int) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timestamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+}
