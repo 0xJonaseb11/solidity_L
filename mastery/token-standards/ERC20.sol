558a9126ae61fa46952bdf4e6ce41f7feb1b0d9c
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-19 14:05:15 +0200

    update mint and burn

--- mastery/token-standards/ERC20.sol
+++ mastery/token-standards/ERC20.sol
@@ -8,4 +8,15 @@ contract MyToken is ERC20 {
     constructor() ERC20("cookie", "cke") {
         _mint(msg.sender, 1000 * 10 ** decimals());
     }
+
+    function mint(uint _qty) public {
+        _mint(msg.sender, _qty);
+    }
+
+    // burn tokens
+    function burn(uint _qty) public {
+        _burn(msg.sender, _qty);
+    }
+
+    
 }
