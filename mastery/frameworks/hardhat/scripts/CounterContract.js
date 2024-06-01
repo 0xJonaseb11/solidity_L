aa5503436a683146cef6adfce4786e29c8fc09c9
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-01 10:45:02 +0200

    update init deploy() --fix

---
+++ mastery/frameworks/hardhat/scripts/CounterContract.js
@@ -0,0 +1,40 @@
+const hre = require("hardhat");
+const ethers = hre.ethers;
+
+const CounterContractABI = require("../artifacts/contracts/Counter.sol/Counter.json");
+const CounterContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+const main = async() => {
+  // get signer
+  const account = await ethers.getSigner("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
+
+  const Counter = await ethers.getContractAtFromArtifact(CounterContractABI, CounterContractAddress);
+
+  try {
+    // fetching initial value of num
+    var num = await Counter.getNum();
+    console.log("The initial value of num is:", num.toString());
+
+    // incrementing num using (first) default account
+    let txIn = await Counter.increment();
+    await txIn.wait();
+    console.log("Num has been incremented by first account!");
+
+    // fetching new value of num
+    num = await Counter.getNum();
+    console.log("The incremented value of num is:", num.toString());
+
+    // decrementing num using (second) account
+    let txDec = await Counter.connect(account).decrement();
+    await txDec.wait();
+    console.log("Num has been decrememted by second account");
+
+    // fetchin new value of num
+    num = await Counter.getNum();
+    console.log("The decremented value of num is:", num.toString());
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+main();
--- mastery/frameworks/hardhat/scripts/deploy.js
+++ mastery/frameworks/hardhat/scripts/deploy.js
@@ -1,40 +1,31 @@
 const hre = require("hardhat");
 const ethers = hre.ethers;
 
-const CounterContractABI = require("../artifacts/contracts/Counter.sol/Counter.json");
-const CounterContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-
 const main = async() => {
-  // get signer
-  const account = await ethers.getSigner("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
+  const signers = await ethers.getSigners();
 
-  const Counter = await ethers.getConctractAtFromArtifact(CounterContractABI, CounterContractAddress);
+  const account = await ethers.getSigner(
+    "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+  );
 
-  try {
-    // fetching initial value of num
-    var num = await Counter.getNum();
-    console.log("The initial value of num is:", num.toString());
+  const Counter = ethers.getContractFactory("Counter");
+  const counter = Counter.deploy();
 
-    // incrementing num using (first) default account
-    let txIn = await Counter.increment();
-    await txIn.wait();
-    console.log("Num has been incremented by first account!");
+  console.log("Deploying Counter contract...")
 
-    // fetching new value of num
-    num = await Counter.getNum();
-    console.log("The incremented value of num is:", num.toString());
+  await counter.deployed();
 
-    // decrementing num using (second) account
-    let txDec = await Counter.connect(account).decrement();
-    await txDec.wait();
-    console.log("Num has been decrememted by second account");
+  console.log("Counter contract deployed to:", counter.address);
 
-    // fetchin new value of num
-    num = await Counter.getNum();
-    console.log("The decremented value of num is:", num.toString());
-  } catch(err) {
-    console.error(err);
-  }
+  const MyContractABI =
+ require("../artifacts/contracts/Counter.sol/Counter.json");
+const MyContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+const MyContract = await ethers.getContractAtFromArtifact(
+  MyContractABI,
+  MyContractAddress
+);
+
 }
 
 main();
