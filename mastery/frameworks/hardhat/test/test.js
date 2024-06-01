6ae2bb5387c78c94fe77b0573888271c010f9a89
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-01 12:32:26 +0200

    debug --tests

--- mastery/frameworks/hardhat/test/test.js
+++ mastery/frameworks/hardhat/test/test.js
@@ -2,20 +2,26 @@ const { expect } = require("chai");
 const hre = require("hardhat");
 const ethers = hre.ethers;
 
-
-const { describe, before } = require("node:test");
-
-describe("Counter Contract", () => {
+describe("Counter Contract", function() {
   let contract;
+
   before(async function() {
+    console.log("Getting contract factory...");
     const Contract = await ethers.getContractFactory("Counter");
+
+    console.log("Deploying contract...");
     contract = await Contract.deploy();
-  })
+
+    console.log("Waiting for contract to be deployed...");
+    await contract.deployed();
+
+    console.log("Contract deployed at:", contract.address);
+  });
 
   it("Should default the value of num to 0", async function() {
-    expect(await contract.getNum()).to.equal(0);
-    // expect(<result>).to.equal(<expected value>)
-
-  })
-
+    console.log("Calling getNum()...");
+    const num = await contract.getNum();
+    console.log("Value of num:", num);
+    expect(num).to.equal(0);
+  });
 });
