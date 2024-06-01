82aff25e9b2a409f57af64047b46116197163eb6
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-01 09:40:30 +0200

    counter --finalized

--- mastery/frameworks/hardhat/scripts/deploy.js
+++ mastery/frameworks/hardhat/scripts/deploy.js
@@ -1,16 +1,40 @@
-const hre = require("Hardhat");
- 
-async function main() {
- const MyContract = await hre.ethers.getContractFactory("MyContract");
- const myContract = await MyContract.deploy();
- 
- await myContract.deployed();
- console.log("Contract address: ", myContract.address);
+const hre = require("hardhat");
+const ethers = hre.ethers;
+
+const CounterContractABI = require("../artifacts/contracts/Counter.sol/Counter.json");
+const CounterContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+const main = async() => {
+  // get signer
+  const account = await ethers.getSigner("0x70997970C51812dc3A010C7d01b50e0d17dc79C8");
+
+  const Counter = await ethers.getConctractAtFromArtifact(CounterContractABI, CounterContractAddress);
+
+  try {
+    // fetching initial value of num
+    var num = await Counter.getNum();
+    console.log("The initial value of num is:", num.toString());
+
+    // incrementing num using (first) default account
+    let txIn = await Counter.increment();
+    await txIn.wait();
+    console.log("Num has been incremented by first account!");
+
+    // fetching new value of num
+    num = await Counter.getNum();
+    console.log("The incremented value of num is:", num.toString());
+
+    // decrementing num using (second) account
+    let txDec = await Counter.connect(account).decrement();
+    await txDec.wait();
+    console.log("Num has been decrememted by second account");
+
+    // fetchin new value of num
+    num = await Counter.getNum();
+    console.log("The decremented value of num is:", num.toString());
+  } catch(err) {
+    console.error(err);
+  }
 }
- 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
- console.error(error);
- process.exitCode = 1;
-});
+
+main();
